3cadd64e4341307b38ea79cc599fa12ffb54f55f
2wodnjs7 <2wodnjs7@gmail.com>
2024-06-26 13:59:50 +0900

    24.06.26. change client.py/server.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from socket import *
 
-serverIP = '172.30.1.8'
-#serverIP = '172.0.0.1'
+#serverIP = '172.30.1.8'
+serverIP = '127.0.0.1'
 serverPort = 80
 
 def create_socket_and_send_message():
@@ -16,7 +16,7 @@ def create_socket_and_send_message():
 
 
 request_message = 'GET /index.html HTTP/1.1\r\n'
-request_message += 'Host: 172.30.1.8:12000\r\n'
-#request_message += 'Host: 172.0.0.1:12000\r\n'
+#request_message += 'Host: 172.30.1.8:12000\r\n'
+request_message += 'Host: 172.0.0.1:12000\r\n'
 
 create_socket_and_send_message()
--- server.py
+++ server.py
@@ -2,10 +2,11 @@ from socket import *
 from datetime import datetime
 import sys
 
+#serverIP = '172.30.1.8'
+serverIP = '127.0.0.1'
 serverPort = 80
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('172.30.1.8', serverPort))
-#serverSocket.bind(('172.0.0.1', serverPort))
+serverSocket.bind((serverIP, serverPort))
 
 serverSocket.listen()
 print('The server is running')
